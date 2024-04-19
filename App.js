beb57dd7e6559a1d3b67338506e7db6d4703fc9d
Gabriel Laguerre <gabriel.g.laguerre@gmail.com>
2024-04-19 11:36:27 -0400

    fixed commit conflicts

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-<<<<<<< HEAD
 import Login from './src/Components/Login';
 import ChooseActions from './src/Components/ChooseActions';
 import CreatePO from './src/Components/CreatePO';
 import CreateReq from './src/Components/CreateReq';
-=======
-import Home from './src/Components/Home';
-import AddUserForm from './src/Components/AddUserForm';
-import { fetchUsers } from './src/Redux/usersSlice';
-
-store.dispatch(fetchUsers())
->>>>>>> origin/createPO
 
 const Stack = createStackNavigator()
 
